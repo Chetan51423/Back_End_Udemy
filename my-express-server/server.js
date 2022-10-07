85740d4d424b84598bd6e1ffe3ecefa35bf7f1ac
Chetan Shinde <94549278+Chetan51423@users.noreply.github.com>
2022-10-08 03:17:40 +0530

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,1 +1,33 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/",function(req, res){
+    res.send("<h1>Hello Chetan!</h1>");
+    
+    console.log(req);
+});
+
+app.get("/contact",function(req, res){
+    res.send("<h1>Hello This is contact page!</h1>");
+    
+    console.log(req);
+});
+
+app.get("/home",function(req, res){
+    res.send("<h1>Hello this is home page</h1>");
+    
+    console.log(req);
+});
+
+app.get("/hobbies",function(req, res){
+    res.send("<h1>Hello this is hobbies page!</h1>");
+    
+    console.log(req);
+});
+
+app.listen(3000,function(){
+    console.log("Server is created on port no. 3000");
+});
 
