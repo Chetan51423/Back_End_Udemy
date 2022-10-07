5a1941d095f75b21257c00c6e1af76e889f77e84
Chetan Shinde <94549278+Chetan51423@users.noreply.github.com>
2022-10-08 03:14:08 +0530

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,32 @@
+const { response } = require("express");
+const express = require("express");
 
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extented: true}));
+
+app.get("/",function(req,res){
+    //res.sendFile("index.html");      this can be the commond to send the perticular file in response
+    res.sendFile(__dirname + "/index.html")
+    
+});
+
+app.post("/", function(req, res){
+
+    var num1=Number(req.body.n1);
+    var num2=Number(req.body.n2);
+    console.log(num1);
+    console.log(num2);
+    
+    var result= num1+num2;
+    //console.log(result);
+
+    res.send("The result of the calculation is " + result); 
+});
+
+app.listen(3000,function(){
+    console.log("Your server is created at port no: 3000");
+
+});
