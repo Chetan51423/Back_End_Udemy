ccbede3f102acfbca8b6fbde31820227da4b4c2b
Chetan Shinde <94549278+Chetan51423@users.noreply.github.com>
2022-10-09 00:39:07 +0530

    Update app.js

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,1 +1,24 @@
+const express = require("express");
 
+const app =express();
+
+ const https = require("https");  // no need to install it through hyper terminal. It is native package in nodejs
+
+
+
+app.get("/", function(req,res){
+    res.send("Server is up and running");
+})
+
+// var url = https//api.openweathermap.org/data/2.5/weather?q=London&appid=eca2b95a622dfece5dc326fb718c0d44;
+
+// https.get(url, function(response){
+//     console.log(response);
+// });
+
+
+
+
+app.listen(000, function(req,res){
+    console.log("Server is running on port 3000");
+})
